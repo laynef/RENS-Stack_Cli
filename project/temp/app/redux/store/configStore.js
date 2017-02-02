c1c74e9c136f225d3646e36d9099da3fbf274079
laynefaler <laynefaler@gmail.com>
2017-02-01 19:33:05 -0800

    minor changes

--- project/temp/app/redux/store/configStore.js
+++ project/temp/app/redux/store/configStore.js
@@ -1,6 +1,7 @@
-import reduxThunk from "redux-thunk";
-import createLogger from "redux-logger";
-import ReduxPromise from "redux-promise";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import ReduxPromise from 'redux-promise'
 
 export default () => (
     createStore(
